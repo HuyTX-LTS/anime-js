f416f03773b79d58ec8b692f3b8121592198ac67
HuyTX-LTS <huytx@ltsgroup.tech>
2025-05-22 17:36:41 +0700

    feat: update package.json for homepage and deploy script, add clsx and tailwind-merge dependencies
    refactor: modify Banner component to use gradient text and remove unused imports
    
    style: enhance index.css with new gradient color variables for navigation and logo
    
    feat: add utility function cn for class name merging using clsx and tailwind-merge
    
    chore: configure Vite base path for deployment
    
    chore: update yarn.lock with new dependencies and versions

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,22 +1,28 @@
 import ToggleSwitchDarkMode from "../ToggleSwitchDarkMode";
-import Logo from "@/assets/logo.png";
-import LogoDark from "@/assets/logo-dark-theme.png";
+// import Logo from "@/assets/logo.png";
+// import LogoDark from "@/assets/logo-dark-theme.png";
 import { HeaderNav } from "./headerNavData";
-import { useThemedImage } from "@/hooks/useThemedImage";
+// import { useThemedImage } from "@/hooks/useThemedImage";
 
 export default function Banner() {
-  const srcLogo = useThemedImage(Logo, LogoDark);
+  // const srcLogo = useThemedImage(Logo, LogoDark);
 
   return (
-    <div className="bg-bg-nav shadow-sm flex items-center justify-center gap-5">
+    <div className="bg-bg-nav shadow-sm flex items-center justify-center gap-5 px-4 py-2">
       <div className="mr-[15%]">
-        <img alt="logo" src={srcLogo} />
+        {/* <img alt="logo" src={srcLogo} /> */}
+        <p className="bg-gradient-to-r from-gradient-logo-nav-l to-pink-300 bg-clip-text text-5xl font-extrabold text-transparent ...">
+          HUY TX
+        </p>
       </div>
 
       <div className="flex items-center justify-self-center gap-7">
         {HeaderNav?.map((v, i) => {
           return (
-            <div key={i}>
+            <div
+              key={i}
+              className="bg-gradient-to-r from-gradient-link-nav-l to-gradient-link-nav-r bg-clip-text text-transparent hover:"
+            >
               <a href={v?.href}>{v?.label}</a>
             </div>
           );
